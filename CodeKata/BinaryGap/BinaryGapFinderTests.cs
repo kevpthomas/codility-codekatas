df5b01d030c17aa0814a1d752d71fd46711e14b9
kevin.thomas <kevin.thomas@equinox.co.nz>
2019-01-22 15:51:42 +1300

    Add initial project setup

---
+++ CodeKata/BinaryGap/BinaryGapFinderTests.cs
@@ -0,0 +1,8 @@
+﻿namespace CodeKata.BinaryGap
+{
+    // https://app.codility.com/programmers/lessons/1-iterations/binary_gap/
+    public class BinaryGapFinderTests
+    {
+        
+    }
+}
---
+++ CodeKata/FakerExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using Bogus;
+// ReSharper disable IdentifierTypo
+
+
+namespace CodeKata
+{
+    public static class FakerExtensions
+    {
+        public static int IntExcept(this Randomizer randomizer, int min = int.MinValue, int max = int.MaxValue, params int[] except)
+        {
+            var value = randomizer.Int(min, max);
+
+            while (except.Contains(value))
+            {
+                value = randomizer.Int(min, max);
+            }
+
+            return value;
+        }
+    }
+}
