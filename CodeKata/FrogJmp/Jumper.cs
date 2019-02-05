41ca06999bf3ca2373b507351562f070fac7955d
kevin.thomas <kevin.thomas@equinox.co.nz>
2019-02-05 16:54:21 +1300

    Solve the FrogJmp codility lesson

---
+++ CodeKata/FrogJmp/Jumper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CodeKata.FrogJmp
+{
+    /// <summary>
+    /// Solves https://app.codility.com/programmers/lessons/3-time_complexity/frog_jmp/
+    /// </summary>
+    public class Jumper
+    {
+        public int JumpCount(int x, int y, int d)
+        {
+            return JumpCount(new JumpInt(x), new JumpInt(y), new JumpInt(d));
+        }
+
+        public int JumpCount(JumpInt x, JumpInt y, JumpInt d)
+        {
+            if (x.Value > y.Value)
+                throw new ArgumentException($"Starting point {x.Value} must not be greater than ending point {x.Value}");
+
+            var distanceToJump = y - x;
+
+            return (int)Math.Ceiling((double)distanceToJump / d.Value);
+        }
+    }
+}
---
+++ CodeKata/FrogJmp/JumperTests.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Ardalis.GuardClauses;
+using NUnit.Framework;
+using Shouldly;
+
+namespace CodeKata.FrogJmp
+{
+    public class JumperTests : UnitTestBase<Jumper>
+    {
+        [TestCase(0)]
+        [TestCase(1000000001)]
+        public void GivenStartingPointOutOfRange(int x)
+        {
+            Should.Throw<ArgumentOutOfRangeException>(
+                () => TestInstance.JumpCount(x, Faker.Random.Int(1, 1000000000), 1));
+        }
+
+        [TestCase(0)]
+        [TestCase(1000000001)]
+        public void GivenEndingPointOutOfRange(int y)
+        {
+            Should.Throw<ArgumentOutOfRangeException>(
+                () => TestInstance.JumpCount(Faker.Random.Int(1, 1000000000), y, 1));
+        }
+
+        [TestCase(0)]
+        [TestCase(1000000001)]
+        public void GivenJumpDistanceOutOfRange(int d)
+        {
+            Should.Throw<ArgumentOutOfRangeException>(
+                () => TestInstance.JumpCount(Faker.Random.Int(1, 1000000000), Faker.Random.Int(1, 1000000000), d));
+        }
+
+        [Test]
+        public void GivenNegativeJumpDistance()
+        {
+            var x = Faker.Random.Int(2, 1000000000);
+            var y = x - 1;
+
+            Should.Throw<ArgumentException>(() => TestInstance.JumpCount(x, y, Faker.Random.Int(1, 1000000000)));
+        }
+
+        [Test]
+        public void GivenJumpDistanceZero()
+        {
+            var x = Faker.Random.Int(1, 1000000000);
+
+            TestInstance.JumpCount(x, x, Faker.Random.Int(1, 1000000000)).ShouldBe(0);
+        }
+
+        [Test]
+        public void GivenExampleJumpDistance()
+        {
+            const int x = 10;
+            const int y = 85;
+            const int d = 30;
+
+            TestInstance.JumpCount(x, y, d).ShouldBe(3);
+        }
+    }
+
+    /// <summary>
+    /// Data object to represent frog jump numbers
+    /// </summary>
+    /// <remarks>
+    /// Avoids primitive obsession
+    /// https://lostechies.com/jimmybogard/2007/12/03/dealing-with-primitive-obsession/
+    /// </remarks>
+    public class JumpInt
+    {
+        public JumpInt(int value)
+        {
+            Guard.Against.OutOfRange(value, nameof(value), 1, 1000000000);
+
+            Value = value;
+        }
+
+        public int Value { get; }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+
+        public static implicit operator int(JumpInt jumpInt)
+        {
+            return jumpInt.Value;
+        }
+
+        public static explicit operator JumpInt(int value)
+        {
+            return new JumpInt(value);
+        }
+    }
+}
