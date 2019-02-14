73d277aeefdb82632cc29e526d48e863cdeec597
kevin.thomas <kevin.thomas@equinox.co.nz>
2019-02-14 16:03:55 +1300

    Shift JumpInt to its own class file.

---
+++ CodeKata/FrogJmp/JumpInt.cs
@@ -0,0 +1,38 @@
+﻿using Ardalis.GuardClauses;
+
+namespace CodeKata.FrogJmp
+{
+    /// <summary>
+    /// Data object to represent frog jump numbers
+    /// </summary>
+    /// <remarks>
+    /// Avoids primitive obsession
+    /// https://lostechies.com/jimmybogard/2007/12/03/dealing-with-primitive-obsession/
+    /// </remarks>
+    public class JumpInt
+    {
+        public JumpInt(int value)
+        {
+            Guard.Against.OutOfRange(value, nameof(value), 1, 1000000000);
+
+            Value = value;
+        }
+
+        public int Value { get; }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+
+        public static implicit operator int(JumpInt jumpInt)
+        {
+            return jumpInt.Value;
+        }
+
+        public static explicit operator JumpInt(int value)
+        {
+            return new JumpInt(value);
+        }
+    }
+}
--- CodeKata/FrogJmp/JumperTests.cs
+++ CodeKata/FrogJmp/JumperTests.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using Ardalis.GuardClauses;
 using NUnit.Framework;
 using Shouldly;
 
@@ -56,40 +55,6 @@ namespace CodeKata.FrogJmp
             const int d = 30;
 
             TestInstance.JumpCount(x, y, d).ShouldBe(3);
-        }
-    }
-
-    /// <summary>
-    /// Data object to represent frog jump numbers
-    /// </summary>
-    /// <remarks>
-    /// Avoids primitive obsession
-    /// https://lostechies.com/jimmybogard/2007/12/03/dealing-with-primitive-obsession/
-    /// </remarks>
-    public class JumpInt
-    {
-        public JumpInt(int value)
-        {
-            Guard.Against.OutOfRange(value, nameof(value), 1, 1000000000);
-
-            Value = value;
-        }
-
-        public int Value { get; }
-
-        public override string ToString()
-        {
-            return Value.ToString();
-        }
-
-        public static implicit operator int(JumpInt jumpInt)
-        {
-            return jumpInt.Value;
-        }
-
-        public static explicit operator JumpInt(int value)
-        {
-            return new JumpInt(value);
         }
     }
 }
